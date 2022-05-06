7b5f32e0fd3a00029764dada306ba031f61dbd77
michellechan40 <102484481+michellechan40@users.noreply.github.com>
2022-05-06 16:32:50 -0600

    Updated apiUrl

--- src/app.js
+++ src/app.js
@@ -13,8 +13,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "44a101a44ae6f5fd6581c9d964176928";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 
