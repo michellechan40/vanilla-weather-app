6b3564266d85d7beb072d9927f19c3bb493f9846
michellechan40 <102484481+michellechan40@users.noreply.github.com>
2022-05-06 16:30:39 -0600

    Added JS structure

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "44a101a44ae6f5fd6581c9d964176928";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric";
+
+console.log(apiUrl);
+
+axios.get(url).then(displayTemperature);
