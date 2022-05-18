51afdaba0ac7bd72f4b90f3a4c953e1d8e26f661
michellechan40 <102484481+michellechan40@users.noreply.github.com>
2022-05-18 08:27:11 -0600

    Added API integration to forecast

--- src/app.js
+++ src/app.js
@@ -1,35 +1,37 @@
 //Date
-let now = new Date();
-let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-let time = now.getHours();
-let minutes = now.getMinutes();
-let currentDay = days[now.getDay()];
+function formatDay(timestamp) {
+  let now = new Date();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let time = now.getHours();
+  let minutes = now.getMinutes();
+  let currentDay = days[now.getDay()];
 
-let h2 = document.querySelector("h2");
-h2.innerHTML = `${currentDay}, ${time}:${minutes}`;
+  let h2 = document.querySelector("h2");
+  h2.innerHTML = `${currentDay}, ${time}:${minutes}`;
+}
 
 //Forecast
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
 
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${forecastDay.dt}</div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max} ° </span>
+          <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}° </span>
         </div>
       </div>
   `;
